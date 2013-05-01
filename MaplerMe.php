8e339066ae7cf1e4c15fef556fb529817558c770
tylerliberman <tylerliberman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-01 19:04:23 +0000

    - Initial Commit - Allows for including of avatars.
    git-svn-id: https://plugins.svn.wordpress.org/maplerme/trunk@706602 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ MaplerMe.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Mapler.me
+Plugin URI: http://mapler.me/
+Description: Mapler.me's Official Wordpress Plugin
+Version: 0.1
+Author: Mapler.me
+Author URI: http://tylerliberman.com
+License: GPL2
+
+Copyright 2013  Tyler Liberman (Mapler.me)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+function maplerme_shortcode( $atts ) {
+
+	extract(shortcode_atts(
+		array(
+			'player' => '',
+			'link' => 'yes',
+		), $atts )
+	);
+
+	if ($link == 'yes') {
+		return '<a href="//mapler.me/player/'.$player.'"><img src="//mapler.me/avatar/'.$player.'"/></a>';
+	}
+	else {
+		return '<img src="//mapler.me/avatar/'.$player.'"/>';
+	}
+}
+add_shortcode( 'maplerme', 'maplerme_shortcode' );
+
+?>
